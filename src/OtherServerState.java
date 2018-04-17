404cfb01503edded0b773a0c332a3ebe267a1ee5
Denis Halilovic <Devilofawesomeness@gmail.com>
2018-04-17 12:14:51 -0400

    Updated states implemented in main operation Inserted bones of leader operation MUst implement queues

--- src/OtherServerState.java
+++ src/OtherServerState.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class OtherServerState {
+    private int nextIndex; // Index of next log entry to send to other server
+    private int matchIndex; // Index of highest log entry known to be replicated other server
+
+    public OtherServerState(int nextIndex) {
+        this.nextIndex = nextIndex;
+        matchIndex = 0;
+    }
+
+    public int getNextIndex() {
+        return nextIndex;
+    }
+
+    public void setNextIndex(int nextIndex) {
+        this.nextIndex = nextIndex;
+    }
+
+    public int getMatchIndex() {
+        return matchIndex;
+    }
+
+    public void setMatchIndex(int matchIndex) {
+        this.matchIndex = matchIndex;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -3,15 +3,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 
 public class main {
 
     private static HashSet<String> ipSet; // Stores IP addresses of fellow nodes
-    private static ArrayList<LogEntry> log;
+    private static int currentTerm; // Latest term server has seen (initialized to 0 on first boot)
+    private static Integer votedFor; // Stores candidateId that received vote in current term (or null if none)
+    private static ArrayList<LogEntry> log; // Stores log entries
+    private static int commitIndex; // Index of highest log entry known to be committed (initialized to 0)
+    private static int lastApplied; // Index of highest log entry applied to state machine (initialized to 0)
+
+    //TODO Implement Client input queue, RPC outbox queue, RPC inbox queue
 
     // Conveys node state
     private enum State {
-        FOLLOWER, CANDIDATE, LEADER;
+        FOLLOWER, CANDIDATE, LEADER
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -25,27 +32,66 @@ public class main {
         while (true) {
             switch (state) {
                 case FOLLOWER:
-                    state = follower();
+                    state = performFollower();
                     break;
                 case CANDIDATE:
-                    state = candidate();
+                    state = performCandidate();
                     break;
                 case LEADER:
-                    state = leader();
+                    state = performLeader();
                     break;
             }
         }
     }
 
-    private static State follower () {
+    private static State performFollower() {
+        // Loop through performFollower operations
+        while (true) {
+            break;
+        }
         return State.CANDIDATE;
     }
 
-    private static State candidate () {
+    private static State performCandidate() {
+        // Loop through performCandidate operations
+        while (true) {
+            break;
+        }
         return State.LEADER;
     }
 
-    private static State leader () {
+    private static State performLeader() {
+        // Initialize volatile state variables (reinitialized after election)
+        LinkedHashMap<String, OtherServerState> otherStates = new LinkedHashMap<String, OtherServerState>();
+        for (String ip : ipSet) {
+            otherStates.put(ip, new OtherServerState(log.size()));
+        }
+
+        // Loop through performLeader operations
+        while (true) {
+            if (commitIndex > lastApplied) {
+                lastApplied++;
+                //TODO Implement applying to log
+                // apply(log.get(lastApplied))
+            }
+
+            //TODO Check if in-queue and out-queue are empty
+            /*
+            if (client input queue has something) {
+                send AppendEntries RPC
+                reset timer
+            } else if (RPC queue has something) {
+                respond appropriately
+            } else if (RPC Response queue has something) {
+                respond appropriately
+            } else {
+                check timer
+            }
+            */
+
+            //TODO replace with further implementation
+            break;
+        }
         return State.FOLLOWER;
     }
 }

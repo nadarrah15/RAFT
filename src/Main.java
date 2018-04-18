87b633c974b952d4642c85922689eccc71d2ce8b
Denis Halilovic <Devilofawesomeness@gmail.com>
2018-04-18 09:35:37 -0400

    Moved majority of operations from Main to Node object

--- src/Main.java
+++ src/Main.java
@@ -1,109 +1,13 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Queue;
 
-public class main {
+public class Main {
 
-    private static HashSet<String> ipSet; // Stores IP addresses of fellow nodes
-    private static int currentTerm; // Latest term server has seen (initialized to 0 on first boot)
-    private static Integer votedFor; // Stores candidateId that received vote in current term (or null if none)
-    private static ArrayList<LogEntry> log; // Stores log entries
-    private static int commitIndex; // Index of highest log entry known to be committed (initialized to 0)
-    private static int lastApplied; // Index of highest log entry applied to state machine (initialized to 0)
+    public static void main(String[] args) throws IOException {
 
-    // TODO Implement Client input queue, RPC outbox queue, RPC inbox
-    // TODO Create class which wraps/unwraps client input, RPC messages
-    // TODO Replace generic types with said wrapper classes
-    private static Queue<Integer> clientInput;
-    private static Queue<Integer> rpcOutbox;
-    private static Queue<Integer> rpcInbox;
-
-    //TODO implement LinkedHashMap of threads handling interaction with other nodes
-    // A: Dedicate one thread to receiving all messages, one per node for sending messages?
-    // B: Have each thread can contain server socket and client socket for one-way connections?
-
-    // Conveys node state
-    private enum State {
-        FOLLOWER, CANDIDATE, LEADER
-    }
-
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-
-        ipSet = new HashSet<String>(Files.readAllLines(Paths.get(args[0]))); // Store IP addresses in .txt file
-        State state = State.FOLLOWER; // Begin life as Follower
-        ClientHandler clientHandler = new ClientHandler(); // Start new thread for console (local client) input
-        clientHandler.start();
-
-        // Commence lifetime operations
-        while (true) {
-            switch (state) {
-                case FOLLOWER:
-                    state = performFollower();
-                    break;
-                case CANDIDATE:
-                    state = performCandidate();
-                    break;
-                case LEADER:
-                    state = performLeader();
-                    break;
-            }
-        }
-    }
-
-    private static State performFollower() {
-        //TODO Implement
-        // Loop through performFollower operations
-        while (true) {
-            break;
-        }
-        return State.CANDIDATE;
-    }
-
-    private static State performCandidate() {
-        //TODO Implement
-        // Loop through performCandidate operations
-        while (true) {
-            break;
-        }
-        return State.LEADER;
-    }
-
-    private static State performLeader() {
-        // Initialize volatile state variables (reinitialized after election)
-        LinkedHashMap<String, OtherServerState> otherStates = new LinkedHashMap<String, OtherServerState>();
-        for (String ip : ipSet) {
-            otherStates.put(ip, new OtherServerState(log.size()));
-        }
-
-        // Loop through performLeader operations
-        while (true) {
-            if (commitIndex > lastApplied) {
-                lastApplied++;
-                //TODO Implement applying to log
-                // apply(log.get(lastApplied))
-            }
-
-            //TODO Check if in-queue and out-queue are empty
-            /*
-            if (client input queue has something) {
-                send AppendEntries RPC
-                reset timer
-            } else if (RPC queue has something) {
-                respond appropriately
-            } else if (RPC Response queue has something) {
-                respond appropriately
-            } else {
-                check timer
-            }
-            */
-
-            //TODO replace with further implementation
-            break;
-        }
-        return State.FOLLOWER;
+        Node node = new Node(new HashSet<String>(Files.readAllLines(Paths.get(args[0]))));
+        node.run();
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,109 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Node {
+
+    private HashSet<String> ipSet; // Stores IP addresses of fellow nodes
+    private int currentTerm; // Latest term server has seen (initialized to 0 on first boot)
+    private Integer votedFor; // Stores candidateId that received vote in current term (or null if none)
+    private ArrayList<LogEntry> log; // Stores log entries
+    private int commitIndex; // Index of highest log entry known to be committed (initialized to 0)
+    private int lastApplied; // Index of highest log entry applied to state machine (initialized to 0)
+    private State state; // Defines follower, candidate, or leader state
+
+    // TODO Implement Client input queue, RPC outbox queue, RPC inbox
+    // TODO Create class which wraps/unwraps client input, RPC messages
+    // TODO Replace generic types with said wrapper classes
+    private Queue<Integer> clientInput;
+    private Queue<Integer> rpcOutbox;
+    private static Queue<Integer> rpcInbox;
+
+    //TODO implement LinkedHashMap of threads handling interaction with other nodes
+    // A: Dedicate one thread to receiving all messages, one per node for sending messages?
+    // B: Have each thread can contain server socket and client socket for one-way connections?
+
+    // Conveys node state
+    private enum State {
+        FOLLOWER, CANDIDATE, LEADER
+    }
+
+    public Node(HashSet<String> ipSet) {
+        this.ipSet = ipSet; // Store IP addresses in .txt file
+        state = State.FOLLOWER; // Begin life as Follower
+        ClientHandler clientHandler = new ClientHandler(); // Start new thread for console (local client) input
+        clientHandler.start();
+    }
+
+    public void run() {
+
+        // Commence lifetime operations
+        while (true) {
+            switch (state) {
+                case FOLLOWER:
+                    state = performFollower();
+                    break;
+                case CANDIDATE:
+                    state = performCandidate();
+                    break;
+                case LEADER:
+                    state = performLeader();
+                    break;
+            }
+        }
+    }
+
+    private State performFollower() {
+        //TODO Implement
+        // Loop through performFollower operations
+        while (true) {
+            break;
+        }
+        return State.CANDIDATE;
+    }
+
+    private State performCandidate() {
+        //TODO Implement
+        // Loop through performCandidate operations
+        while (true) {
+            break;
+        }
+        return State.LEADER;
+    }
+
+    private State performLeader() {
+        // Initialize volatile state variables (reinitialized after election)
+        LinkedHashMap<String, OtherServerState> otherStates = new LinkedHashMap<String, OtherServerState>();
+        for (String ip : ipSet) {
+            otherStates.put(ip, new OtherServerState(log.size()));
+        }
+
+        // Loop through performLeader operations
+        while (true) {
+            if (commitIndex > lastApplied) {
+                lastApplied++;
+                //TODO Implement applying to log
+                // apply(log.get(lastApplied))
+            }
+
+            //TODO Check if in-queue and out-queue are empty
+            /*
+            if (client input queue has something) {
+                send AppendEntries RPC
+                reset timer
+            } else if (RPC queue has something) {
+                respond appropriately
+            } else if (RPC Response queue has something) {
+                respond appropriately
+            } else {
+                check timer
+            }
+            */
+
+            //TODO replace with further implementation
+            break;
+        }
+        return State.FOLLOWER;
+    }
 }

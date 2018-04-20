22f8ed2532bc2d80777268f9e0d2157d4f593c8e
nadarrah15 <nadarrah15@gmail.com>
2018-04-20 09:14:44 -0400

    Net Class

---
+++ src/Net.java
@@ -0,0 +1,12 @@
+public class Net {
+
+    private Node node;
+
+    public void listen(int p) {
+        while (true) {
+            //1. launch a thread that listens on port p
+            //2. when a connection comes in,
+            //2a. launch a thread to receive the message and send it to the node queue
+        }
+    }
+}

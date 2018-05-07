5ec2e166bc369b5dde0b0a92acb0e077809a4fbe
Nathan Darraj <nadarrah15@gmail.com>
2018-05-07 17:41:04 -0400

    fixing RequestVoteResponse

--- src/Client.java
+++ src/Client.java
@@ -4,13 +4,20 @@ public class Client {
 
     public static void main(String[] args){
         while(true){
-            System.out.print("Enter in request or q to quit: ");
+            System.out.print("Enter request type or q to quit: ");
             Scanner in = new Scanner(System.in);
-            String s = in.nextLine();
+            String request = in.nextLine();
 
-            if(s.equalsIgnoreCase("q"))
+            if(request.equalsIgnoreCase("q"))
                 break;
+            else if(!request.equalsIgnoreCase("append") && !request.equalsIgnoreCase("delete")) {
+                System.out.print("I do not recognize that request. Please try again. ");
+                continue;
+            }
 
+            System.out.print("Enter the arguement: ");
+
+            QueueEntry queueEntry = new QueueEntry(QueueEntry.Type.Input, request + in.nextLine());
 
         }
     }

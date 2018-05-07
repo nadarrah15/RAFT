0f9177a8ea41af93cd0ff9620b8b2b1fbc74663a
Devilofawesomeness <devilofawesomeness@gmail.com>
2018-05-07 12:47:58 -0400

    Updated ipSet creation

--- src/Node.java
+++ src/Node.java
@@ -65,7 +65,7 @@ public class Node {
                     state = performFollower();
                     break;
                 case CANDIDATE:
-                    System.out.println("[NODE] State -> candidate");
+                    //System.out.println("[NODE] State -> candidate");
                     state = performCandidate();
                     break;
                 case LEADER:
